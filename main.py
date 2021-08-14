1d408f4f6d5ed037eab715a12177310c7c4b33a8
alanoneill100 <alan2oneill@gmail.com>
2021-08-14 19:09:36 +0100

    Analysed dataframe to begin cleaning

--- main.py
+++ main.py
@@ -7,5 +7,13 @@ marketing_data = pd.read_csv("marketing_data.csv", index_col=0)
 #subset of csv volumns printed below
 print(marketing_data)
 
+#showing type below (dataframe)
+print(type(marketing_data))
+
+#Analysing dataframe
+print(marketing_data.head())
+print(marketing_data.shape)
+#2,240 rows and 27 columns
+
 
 
