66a97f3b75f79165706fc9627af3340467f133fb
alanoneill100 <alan2oneill@gmail.com>
2021-08-14 18:40:15 +0100

    imported pandas package to_main.py; imported csv file to pycharm

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("test commit")
+#importing pandas
+
+import pandas as pd
+
+#reading cdv file imported from kaggle (first comumn removed as not needed)
+marketing_data = pd.read_csv("marketing_data.csv", index_col=0)
+#subset of csv volumns printed below
+print(marketing_data)
+
+
+
