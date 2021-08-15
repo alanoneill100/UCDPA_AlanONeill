96f16f2029bf20ad5f2a7c9ab78da8dc787db165
alanoneill100 <alan2oneill@gmail.com>
2021-08-15 16:07:10 +0100

    Identified missing values and replaced them with mean values of dataset

--- main.py
+++ main.py
@@ -3,17 +3,31 @@
 import pandas as pd
 
 #reading cdv file imported from kaggle (first comumn removed as not needed)
-marketing_data = pd.read_csv("marketing_data.csv", index_col=0)
+df_marketing_data = pd.read_csv("marketing_data.csv", index_col=0)
 #subset of csv volumns printed below
-print(marketing_data)
+print(df_marketing_data)
 
 #showing type below (dataframe)
-print(type(marketing_data))
+print(type(df_marketing_data))
 
 #Analysing dataframe
-print(marketing_data.head())
-print(marketing_data.shape)
+print(df_marketing_data.head())   #prints first 5 columns
+print(df_marketing_data.shape)
 #2,240 rows and 27 columns
 
+#Missing values
+print(df_marketing_data.info())
+print (df_marketing_data.isna().sum())
 
+#Option 1 - Drop missing values
+#cleaned_df_marketing_data = df_marketing_data.dropna()
+#cleaned_df_marketing_data = df_marketing_data.dropna(axis=1)
+#print(df_marketing_data.shape, cleaned_df_marketing_data.shape)
 
+#Option 2 - Replace missing values
+cleaned_df_marketing_data = df_marketing_data.fillna(0)
+#print(df_marketing_data.shape, cleaned_df_marketing_data.shape)
+print (cleaned_df_marketing_data.isna().sum())
+print (df_marketing_data.mean())
+
+print (cleaned_df_marketing_data.isna().sum())
